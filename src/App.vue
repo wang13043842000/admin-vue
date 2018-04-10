97534577aa8bcc987a5bbef59124934e5352cffd
123 <123.qq.com>
2018-04-10 20:38:33 +0800

    添加登陆和home组件并配置路由导航

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="app">
     <p>app component</p>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  
+  name: 'app'
 }
 </script>
 
