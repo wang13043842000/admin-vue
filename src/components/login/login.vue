09012c1eca20c8529d8931805d646f45599e6607
123 <123.qq.com>
2018-04-10 21:19:54 +0800

    简单结合element框架使用

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,31 +1,42 @@
 <template>
-    <div>
-        <p>登陆组件</p>
-        <form >
-            <div>
-                <label for="username">用户名</label>
-                <input type="text" id="username" v-model="user.username">
-            </div>
-            <div>
-                <label for="password">密码</label>
-                <input type="password" id="password">
-            </div>
-            <div><button>点击登录</button></div>
-        </form>
+    <div class="login-wrap">
+        <el-form lable-position = "top" ref="form" :model="userForm" label-width="80px">
+            <el-form-item label="用户名">
+                <el-input v-model="userForm.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input type="password" 
+                v-model="userForm.password"></el-input>
+            </el-form-item>
+             <el-form-item>
+                <el-button type="primary" @click="login">登陆</el-button>
+                <el-button>取消</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
     data () {
         return {
-            user: {
+            userForm: {
                 username: '',
                 password: '',
             }
         }
     },
     methods: {
-        
+        async login () {
+            const res = await axios.post('http://localhost:8888/api/private/v1/login',this.userForm)
+            if(res.data.meta.status===200){
+                console.log(res)
+                // 如果返回的数据里面的status等于200  登陆成功跳转
+                this.$router.push({
+                    name: 'home'
+                })
+            }
+        }
     }
   
 }
